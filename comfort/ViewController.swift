24017970a93cc8249ac414bf0195795bb80aea52
Pichugin Dmitry <pichugind@gmail.com>
2018-06-14 16:42:03 +0300

    rtrtrt

--- comfort/ViewController.swift
+++ comfort/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var mapImageView: UIImageView!
     
+    
+        var points:[(Float,Float,String)] = [(65,8,"0:a6:ca:10:8a:42"),(65,7,"0:a6:ca:10:8a:4d"),(47,11,"0:a6:ca:10:b0:e2"),(95,28,"95,28,0:a6:ca:2d:7b:d2"),(97,20,"0:a6:ca:35:93:f2"),(41,11,"0:a6:ca:56:42:7d"),(89,12,"0:a6:ca:56:c:92"),(83,10,"0:a6:ca:56:c:e2"),(94,18,"0:a6:ca:56:e:72"),(75,9,"0:a6:ca:68:9d:a2"),(45,8,"0:a6:ca:68:a3:fd")]
+    
     private var currentlyConnectedMacAdress:String? {didSet{
         if let mac = currentlyConnectedMacAdress {
             historyMacAdresses.append(mac)
